1ab2a4a87a5801488db14ff68dcbe4a35a3e2cf8
천명현 <cmh1448@naver.com>
2024-12-30 14:31:46 +0900

    feat(be): 백엔드 스프링 부트 프로젝트로 변경

---
+++ backend/src/test/kotlin/github/cmh1448/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package github.cmh1448.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
